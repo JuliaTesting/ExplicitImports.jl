9abe1580a00f134e58a9ee3689fe0a4b1c6530e2
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2025-07-19 18:35:41 +0200

    Move vendored code into src, fix version (#126)
    * move vendored source code into src
    
    * rm extra files
    
    * up
    
    * fix version
    
    * rm stray files

--- vendor/run.jl
+++ vendor/run.jl
@@ -7,6 +7,11 @@ for (name, version)  in deps
     pkg = find_package(name; version)
     local_path, reachable, _ = PackageAnalyzer.obtain_code(pkg)
     @assert reachable
-    p = mkpath(joinpath(@__DIR__, name, "src"))
-    cp(joinpath(local_path, "src"), p; force=true)
+    p = mkpath(joinpath(@__DIR__, "..", "src", "vendored", name))
+    # remove any existing files
+    if isdir(p)
+        rm(p; recursive=true, force=true)
+    end
+    mkpath(joinpath(p, "src"))
+    cp(joinpath(local_path, "src"), joinpath(p, "src"); force=true)
 end
