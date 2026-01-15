75b69293e48de74a1cf95a41866ce59d0688d081
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2026-01-15 15:15:40 +0100

    more tests

--- test/issue_81.jl
+++ test/issue_81.jl
@@ -18,3 +18,43 @@ function register_steelProfile()
 end
 
 end # module CmdInterpolationUsesImport
+
+module CmdInterpolationUsesImportQuoted
+
+using ..UnzipExporter81: unzip
+
+function register_quoted(file)
+    run(`"$(unzip())" -q "$file"`)
+end
+
+end # module CmdInterpolationUsesImportQuoted
+
+module CmdInterpolationUsesImportNested
+
+using ..UnzipExporter81: unzip
+
+function register_nested(file)
+    run(`$(unzip()) -q $(joinpath(dirname(file), basename(file)))`)
+end
+
+end # module CmdInterpolationUsesImportNested
+
+module CmdInterpolationUsesImportAdjacent
+
+using ..UnzipExporter81: unzip
+
+function register_adjacent(file)
+    run(`$(unzip()) --dest=$(basename(file))_out`)
+end
+
+end # module CmdInterpolationUsesImportAdjacent
+
+module CmdInterpolationUsesImportQuotedLiteral
+
+using ..UnzipExporter81: unzip
+
+function register_quoted_literal(file)
+    run(`echo '$unzip' $file`)
+end
+
+end # module CmdInterpolationUsesImportQuotedLiteral
